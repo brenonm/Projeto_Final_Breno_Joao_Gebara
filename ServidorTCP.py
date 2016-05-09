5c2c18c1f23946f4de77e735185ed4852e5e3ed8
JoaoRezende <joaorezende@MacBook-Pro-de-Joao.local>
2016-05-09 16:46:18 -0300

    correção conexão externa

--- ServidorTCP.py
+++ ServidorTCP.py
@@ -1,11 +1,12 @@
 import socket
-host= '127.0.0.1' #Servidor interno
+host= '0.0.0.0' #Servidor interno
 porta= 5000 #Porta de comunicação
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Define metodo como TCP (SOCK_STREAM)
 orig=(host,porta)
 tcp.bind(orig)
 tcp.listen(1)
 con, cliente = tcp.accept()
+print("OK!")
 while True:
 	msg=con.recv(1024)  #Estabelece mensagem recebida em 1024 bytes
 	msg.decode('utf-8')
