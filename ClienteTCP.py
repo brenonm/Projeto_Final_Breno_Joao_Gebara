082a86ddf546c753e715fcbde2393ad75eea062c
JoaoRezende <joaorezende@MacBook-Pro-de-Joao.local>
2016-05-09 16:46:08 -0300

    alterações para funcionamento callback

--- ClienteTCP.py
+++ ClienteTCP.py
@@ -1,13 +1,11 @@
 import socket
 host = '127.0.0.1'     # Endereco IP do Servidor
-porta = 5000            # Porta que o Servidor esta
+porta = 5000          # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (host, porta)
 tcp.connect(dest)
 while True:
-	msg= input('Mensagem: ')
-	tcp.send(msg.encode('utf-8'))
 	msg = tcp.recv(1024)
 	msg.decode('utf-8')
-	print(host, ':', msg)
+	print(msg)
 
