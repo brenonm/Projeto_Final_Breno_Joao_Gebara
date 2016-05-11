5e785f2862821dc4df5069f40257f5367e9066ca
Eduardo Gebara <dugebara@uol.com.br>
2016-05-11 13:54:02 -0300

    pagina inicial

--- testepaginicial.py
+++ testepaginicial.py
@@ -13,22 +13,21 @@ class Pagina_Inicial:
         self.window.title("Batalha Naval")
         self.window.geometry("400x400")
 
+        # foto = ImageTk.PhotoImage(Image.open("titulo.gif"))
+        # panel = tk.Label(self.window, image = foto)
+        # panel.pack(side = "bottom", fill ="both", expand = "yes")
 
-		#foto = ImageTk.PhotoImage(Image.open("titulo.gif"))
-		#panel = Label(root, image = foto)
-		#panel.pack(side = "bottom", fill = "both", expand = "yes")
+        titulo=tk.Label(self.window, text="B4T4LH4 N4V4L", font=("Helvetica", 30))
+        titulo.pack(side = "top",fill="both", expand="yes")
 
-        titulo=tk.Label(self.window, text="B4T4LH4 N4V4L", font=("Helvetica", 26))
-        titulo.pack()
-
-        self.check=tk.Button(self.window, text="servidor")
-        self.check2=tk.Button(self.window, text="cliente")
+        self.check=tk.Button(self.window, text="servidor", font=("Helvetica", 26))
+        self.check2=tk.Button(self.window, text="cliente", font=("Helvetica", 26))
         cmd=lambda: self.callback2()
         self.check.configure(command=cmd)
         cmd2=lambda: self.callback()
         self.check2.configure(command=cmd2)
-        self.check.pack()
-        self.check2.pack()
+        self.check.pack(side = "top", fill = "both", expand = "yes")
+        self.check2.pack(side = "top", fill = "both", expand = "yes")
 		
 		
     def iniciar(self):
@@ -37,16 +36,16 @@ class Pagina_Inicial:
 	
     def callback(self):
         entrada_ip=tk.Entry(self.window)
-        entrada_ip.pack()
-        conectar=tk.Button(self.window, text="Conectar")
-        conectar.pack()
+        entrada_ip.pack(side = "top", expand = "yes")
+        conectar=tk.Button(self.window, text="Conectar", font=("Helvetica", 26))
+        conectar.pack(side = "top", expand = "yes")
         self.check.pack_forget()
         self.check2.pack_forget()
 		
 
     def callback2(self):
-        aguardando=tk.Label(self.window, text="Aguardando...")
-        aguardando.pack()   
+        aguardando=tk.Label(self.window, text="Aguardando...", font=("Helvetica", 26))
+        aguardando.pack(side = "top", fill = "both", expand = "yes")   
         self.check.pack_forget()
         self.check2.pack_forget()
 			
