52b1d5ef2e049d990d4213d65eb5177105719f0c
Eduardo Gebara <dugebara@uol.com.br>
2016-05-11 16:51:15 -0300

    atualização

--- TF Classe Tabuleiro.py
+++ TF Classe Tabuleiro.py
@@ -19,47 +19,56 @@ class Pagina_Inicial:
 
         self.window=tk.Tk()
         self.window.title("Batalha Naval")
-        self.window.geometry("200x200")
+        self.window.geometry("400x400")
 
 
         #foto = ImageTk.PhotoImage(Image.open("titulo.gif"))
         #panel = Label(root, image = foto)
         #panel.pack(side = "bottom", fill = "both", expand = "yes")
 
-        titulo=tk.Label(self.window, text="B4T4LH4 N4V4L", font=("Helvetica", 16))
-        titulo.pack()
+        titulo=tk.Label(self.window, text="B4T4LH4 N4V4L", font=("Helvetica", 26))
+        titulo.pack(side = "top",fill="both", expand="yes")
 
-        self.check=tk.Button(self.window, text="servidor")
-        self.check2=tk.Button(self.window, text="cliente")
+        self.check=tk.Button(self.window, text="servidor", font=("Helvetica", 26))
+        self.check2=tk.Button(self.window, text="cliente", font=("Helvetica", 26))
         cmd=lambda: self.callback2()
         self.check.configure(command=cmd)
         cmd2=lambda: self.callback()
         self.check2.configure(command=cmd2)
-        self.check.pack()
-        self.check2.pack()
+        self.check.pack(side = "top", fill = "both", expand = "yes")
+        self.check2.pack(side = "top", fill = "both", expand = "yes")
         
         
     def iniciar(self):
         
         self.window.mainloop()
+
+      
     
     def callback(self):
-        entrada_ip=tk.Entry(self.window)
-        entrada_ip.pack()
-        conectar=tk.Button(self.window, text="Conectar")
-        conectar.pack()
+        self.entrada_ip=tk.Entry(self.window)
+        self.entrada_ip.pack(side = "top", expand = "yes")
+        self.conectar=tk.Button(self.window, text="Conectar", font=("Helvetica", 26))
+        cmd3=lambda: self.pegarip()
+        self.conectar.configure(command=cmd3)
+        self.conectar.pack(side = "top", expand = "yes")
         self.check.pack_forget()
         self.check2.pack_forget()
-        host = entrada_ip     # Endereco IP do Servidor
+        self.server = 0     # Endereco IP do Servidor
         porta = 5000            # Porta que o Servidor esta
         tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        dest = (host, porta)
-        tcp.connect(dest)
-        
+        dest = (self.server, porta)
+                
+    def pegarip(self):
+        #self.entrada_ip.get()
+        self.server=self.entrada_ip  
+        return self.server
+
+
 
     def callback2(self):
-        aguardando=tk.Label(self.window, text="Aguardando...")
-        aguardando.pack()   
+        self.aguardando=tk.Label(self.window, text="Aguardando...", font=("Helvetica", 26))
+        self.aguardando.pack(side = "top", fill = "both", expand = "yes")   
         self.check.pack_forget()
         self.check2.pack_forget()
         host= '127.0.0.1' #Servidor interno
